55028a1930ede3a35ff9d03e1cf83433a04b24a8
JL <jlsangir@gmail.com>
2021-05-31 11:07:36 +0200

    Se añade ADIOS

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-    print("Cómo estás?")
+    print("Cómo estás? Yo bien")
 
+def print_by(name):
+    print("Adios")
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('Jose Luis')
+    print_by("PP")
