73551d848c39ca69f72d9ef7f1f37337bfaa8e92
JL <jlsangir@gmail.com>
2021-05-31 10:50:32 +0200

    Se cambia el printal subproceso

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print("Cómo estás?")
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('Jose Luis')
-    print("Cómo estás?")
-
