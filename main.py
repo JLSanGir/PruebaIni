7d7258778ee421924fdffe7ee623a0986e02be49
jlsanCasa <jlsangir@gmail.com>
2021-06-01 17:38:41 +0200

    En casa. Añado fecha

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from datetime import date
+from datetime import datetime
+
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
@@ -8,8 +11,15 @@ def print_by(name):
 def print_preg(name):
     print(f"Cómo estás, {name}? Yo bien")
 
+def print_ver(fecha):
+    print(f"Realizado {fecha}")
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('Jose Luis')
     print_preg("Sara")
     print_by("Javier")
+
+    today = date.today()
+
+    print_ver(today)
