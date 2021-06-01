201d91bb774e059a267f71ed23ddffdd6de9863e
JL <jlsangir@gmail.com>
2021-06-01 10:53:37 +0200

    Se pregunta sobre la salud

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-    print("Cómo estás? Yo bien")
 
 def print_by(name):
     print("Adios " + name)
 
+def print_preg(name):
+    print("Cómo estás? Yo bien")
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('Jose Luis')
-    print_by("PP")
+    print_preg("Sara")
+    print_by("Javier")
